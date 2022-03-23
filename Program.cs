c1e111c8d14565aec153b040bd286b99c783c63d
xxxZino <101564172+xxxZino@users.noreply.github.com>
2022-03-23 19:20:11 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat object taxi
+            taxii taxi = new taxii();
+
+            //pengestan nilai propertis
+            taxi.drivername = "Jono";
+            taxi.onduty = true;
+            taxi.numpassenger = 10;
+
+            //pemanggilan method
+            taxi.taxiinfo();
+            taxi.pickuppassenger();
+            taxi.dropoffpassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ taxii.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxi
+{
+    class taxii
+    {
+        public string drivername { get; set; }
+        public bool onduty { get; set; }
+        public int numpassenger { get; set; }
+
+        private string ondutyyatdk()
+        {
+            return onduty ? "yes" : "no";
+        }
+
+        public void taxiinfo()
+        {
+            Console.WriteLine("driver name: {0}", drivername);
+            Console.WriteLine("on duty: {0}", ondutyyatdk());
+            Console.WriteLine("number of passenger: {0}", numpassenger);
+        }
+        public void pickuppassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", drivername);
+        }
+        public void dropoffpassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", drivername);
+        }
+    }
+}
